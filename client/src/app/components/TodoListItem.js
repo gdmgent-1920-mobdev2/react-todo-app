20f4dfc46e6438e7ddc6607779d66a5892a94047
drdynscript <drdynscript@gmail.com>
2020-02-27 11:13:42 +0100

    Add CompletedTodo method to the TodoContectProvider and consume it in TodoListItem

--- client/src/app/components/TodoListItem.js
+++ client/src/app/components/TodoListItem.js
@@ -1,15 +1,17 @@
 import React, { useContext } from 'react';
+import classnames from 'classnames';
+
 import { TodoContext } from '../context';
 
 import './TodoListItem.css';
 
 const TodoListItem = ({todo}) => {
-    const { removeTodo } = useContext(TodoContext);
+    const { completeTodo, removeTodo } = useContext(TodoContext);
 
     return (
-        <li className="todo-list__item">
+        <li className={classnames('todo-list__item', (todo.completed) ? 'todo-list__item--completed' : '')}>
             <h1>{todo.title}</h1>
-            <span className="btn-complete">C</span>
+            <span className="btn-complete" onClick={ev => completeTodo(todo.id, !todo.completed)}>C</span>
             <span className="btn-remove" onClick={ev => removeTodo(todo.id)}>R</span>
         </li>
     );
--- client/src/app/context/TodoContext.js
+++ client/src/app/context/TodoContext.js
@@ -4,20 +4,24 @@ import uuid from 'uuid/v1';
 const TodoContext = createContext();
 
 const mockData = [
-    { id: uuid(), title: 'Mijn kindjes wassen', completed: false, createdAt: Date.now() },
+    { id: uuid(), title: 'Mijn kindjes wassen', completed: true, createdAt: Date.now() },
     { id: uuid(), title: 'Vegan food halen uit den Albert', completed: true, createdAt: Date.now() },
     { id: uuid(), title: 'Nieuw Macbook Pro 18inch afhalen at coolblue', completed: false, createdAt: Date.now() }
 ];
 
 const TodoContextProvider = ({children}) => {
-    const [ todos, setTodos ] = useState(mockData);
+    const [ todos, setTodos ] = useState((localStorage.getItem('todoapp.todo')) ? JSON.parse(localStorage.getItem('todoapp.todo')) : []);
+
+    useEffect(() => {
+        localStorage.setItem('todoapp.todo', JSON.stringify(todos));
+    }, [todos])
 
     const addTodo = (title) => {
         setTodos([
             ...todos,
             {
                 id: uuid(),
-                title: title,
+                title,
                 completed: false,
                 createdAt: Date.now()
             }
@@ -28,8 +32,12 @@ const TodoContextProvider = ({children}) => {
         setTodos(todos.filter(todo => todo.id !== id));
     };
 
+    const completeTodo = (id, completed) => {
+        setTodos([...todos.map((todo) => (todo.id === id) ? {...todo, completed } : todo)]);
+    };
+
     return (
-        <TodoContext.Provider value={{todos, addTodo, removeTodo}}>
+        <TodoContext.Provider value={{todos, addTodo, completeTodo, removeTodo}}>
             {children}
         </TodoContext.Provider>
     )
